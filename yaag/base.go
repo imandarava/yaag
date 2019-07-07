c2d659f50f5153465fcf264467db2990d1cfe59f
mandarava <927395536@qq.com>
2019-07-07 17:39:39 +0800

    init

--- yaag/base.go
+++ yaag/base.go
@@ -6,10 +6,10 @@ const Template = `<!DOCTYPE html>
     <meta charset="UTF-8">
     <title> API Documentation </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>   
+    <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>   
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <style type="text/css">
